c58cb6bc9ad44eab6f73ecf27eb9ddcf931fde6e
Danny <dannyespinoza819@gmail.com>
2026-05-27 17:08:50 -0500

    Mi primer versionamiento en Spring Boot

---
+++ src/main/java/com/distrbuida/libreria/LibreriaApplication.java
@@ -0,0 +1,17 @@
+package com.distrbuida.libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaApplication.class, args);
+
+		System.out.println("HOLA MUNDO SPRING BOOT...!!");
+
+
+	}
+
+}
